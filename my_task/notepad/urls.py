ca332bbcecbf79cbdc37ec7eac3632bf02c17016
sumonghosh825 <sumonghosh892@gmail.com>
2025-05-05 20:34:56 +0530

    ok

--- my_task/notepad/urls.py
+++ my_task/notepad/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import (
     register, user_login, user_logout, home, 
-    task_management, add_task, edit_task, delete_task, 
-    profile, update_profile, load_security_content, update_security, 
-    load_tasks_by_category,load_tasks_by_status, load_profile_content
+    
 )
 
 urlpatterns = [
